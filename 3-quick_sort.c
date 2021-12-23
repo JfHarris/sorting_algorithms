db1d96b2b960b2b061dea7d9de2a596b99a6582a
JfHarris <3374@holbertonschool.com>
2021-12-22 21:48:26 -0800

    Add/edit

---
+++ 3-quick_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sort array using quick sort algo
+ *@array: array of ints
+ *@size: array size
+ *
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	(void) size;
+	(void) array;
+	printf("Hello/n");
+}
