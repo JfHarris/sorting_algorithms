3dc0e30daea5e03c166a01aec41dea213a80496a
JfHarris <3374@holbertonschool.com>
2021-12-22 21:51:42 -0800

    Add/Edit

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts list in ascending order using insertion sort
+ *@list: doubliy linked list
+ *
+ * Return: Always 0
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	(void) list;
+}
