95288ce3b4fbeac9cd95fc2d86e1bfe1fa62338d
JfHarris <3374@holbertonschool.com>
2021-12-23 19:57:55 -0800

    Starting over

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,32 +9,5 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
-	listint_t *tmp;
-	listint_t *tmp1;
-
-	if (list != NULL || *list != NULL)
-	{
-		current = *list;
-		tmp = current->next;
-		if (tmp == NULL)
-			return;
-		while (tmp != NULL)
-		{
-			if (current->prev != NULL && current->n > tmp->n)
-			{
-				if (current->prev->n < tmp->n)
-					break;
-				current = current->prev;
-			}
-			tmp1 = current;
-			current = tmp;
-			tmp = tmp1;
-			tmp = current->next;
-			print_list(*list);
-		}
-		if (current->next != NULL)
-			current = current->next;
-		tmp = current->next;
-	}
+	(void) list;
 }
