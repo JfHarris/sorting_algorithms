ff41e2b462ab89f32712688d7181ba9aec33750e
jfharris <jfharris1983@gmail.com>
2022-05-03 01:11:39 -0500

    Added merge sort prototype to sort.h

---
+++ 103-merge_sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * merge_sort - top-down merge sort algorithm
+ *
+ * Return: Always 0
+ */
+
+void merge_sort(int *array, size_t size)
+{
+    
+}
