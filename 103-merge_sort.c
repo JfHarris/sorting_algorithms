2a0c4df3b7734455995e4a24a76b69078318772d
jfharris <jfharris1983@gmail.com>
2022-05-08 23:08:14 -0500

    3 days later...

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,62 +3,108 @@
 #include "sort.h"
 
 /**
-* init_merge_sort - top-down merge sort algorithm
-* @array: array to sort
-* @temp: array to hold info
-* @size: array size
-*/
-
-void init_merge_sort(int *array, int *temp, size_t size)
+ *init_merge_sort - merge sort algorithm
+ *@array: int array pointer
+ *@first: size of array
+ *@middle: size of array
+ *@last: size of array
+ *@dest: int array pointer
+ */
+void init_merge_sort(int *array, size_t first, size_t middle, size_t last, int *dest)
 {
-	size_t half = size / 2;
-	size_t x, y = 0;
+	size_t x = first;
+	size_t y = middle;
 	size_t z;
 
-	if (size < 2)
-		return;
-
-	init_merge_sort(array, temp, half);
-	init_merge_sort(array + half, temp + half, size - half);
-
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(array, half);
-	printf("[right]: ");
-	print_array(array + half, size - half);
-	for (z = 0; z < size; z++)
-		if (y >= size - half || (x < half && array[x] < (array + half)[y]))
+	for (z = first; z < last; z++)
+	{
+		if (x < middle && (y >= last || array[x] <= array[y]))
 		{
-			temp[z] = array[x];
-			x++;
+			dest[z] = array[x];
+			x = x + 1;
 		}
 		else
 		{
-			temp[z] = (array + half)[y];
-			y++;
+			dest[z] = array[y];
+			y = y + 1;
 		}
-	for (z = 0; z < size; z++)
-		array[z] = temp[z];
-	printf("[Done]: ");
-	print_array(array, size);
+	}
 }
 
 /**
-* merge_sort - initiate merge sort
-* @array: array to be sorted
-* @size: size of the array
-*/
+ *split - output format
+ *@dest: int array pointer
+ *@first: size of array
+ *@last: size of array
+ *@array: int array pointer
+ */
+
+void split(int *dest, size_t first, size_t last, int *array)
+{
+	size_t middle;
+	size_t x;
+
+	if (last - first < 2)
+		return;
+	middle = (last + first) / 2;
+	split(array, first, middle, dest);
+	split(array, middle, last, dest);
+	init_merge_sort(dest, first, middle, last, array);
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	for (x = first; x < middle; x++)
+	{
+		printf("%d", dest[x]);
+		if (x < middle - 1)
+			printf(", ");
+	}
+	printf("\n[right]: ");
+	for (x = middle; x < last; x++)
+	{
+		printf("%d", dest[x]);
+		if (x < last - 1)
+			printf(", ");
+	}
+	printf("\n[Done]: ");
+	for (x = first; x < last; x++)
+	{
+		printf("%d", array[x]);
+		if (x < last - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
+
+/**
+ *set_array - setting array contents
+ *@array: int array pointer
+ *@dest: destination array
+ *@size: size of array
+ */
+
+void set_array(int *array, int *dest, size_t size)
+{
+	size_t site = 0;
+
+	for (site = 0; site < size; site++)
+	{
+		dest[site] = array[site];
+	}
+}
+
+/**
+ *merge_sort - sorts array of ints in ascending order using Merge sort algo
+ *@array: int array pointer
+ *@size: size of array
+ */
+
 void merge_sort(int *array, size_t size)
 {
-	int *temp;
+	int *dest = malloc(sizeof(int) * size);
 
-	if (!array || size < 2)
-		return;
-
-	temp = malloc(sizeof(*temp) * size);
-	if (!temp)
-		return;
-
-	init_merge_sort(array, temp, size);
-	free(temp);
+	set_array(array, dest, size);
+	split(dest, 0, size, array);
+	free(dest);
 }
